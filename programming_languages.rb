47327273ca3321d8f5847fe9bf71c46ff0cfbe55
Brad Newman <newmanbradm@gmail.com>
2018-05-19 23:30:19 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   languages.each do |oo_or_functional, language_hash|
     language_hash.each do |language, attributes_hash|
       attributes_hash.each do |attribute, string|
-        
+        if attribute[language]
   
   
   new_hash
